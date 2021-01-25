9d97d93362441295508fe9181a886d467c1e4f1d
neurophysiology77 <77329599+neurophysiology77@users.noreply.github.com>
2021-01-25 10:29:54 +0000

    Update task-3.1.2.js
    Add alert, console.log and document.write

---
+++ module-3/lesson-1/scripts/task-3.1.2.js
@@ -0,0 +1,8 @@
+alert('This is an alert message'); //This is a pop-up message box
+console.log('This is a message from the console outer space');
+/*
+Below is a message that will
+appear in the console
+*/
+
+document.write('Hello from the document');
