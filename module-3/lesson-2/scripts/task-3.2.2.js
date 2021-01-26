4876bc2ca5d511cd753e27d57b3fb5b3fd59af5a
neurophysiology77 <77329599+neurophysiology77@users.noreply.github.com>
2021-01-26 10:33:24 +0000

    Update task-3.2.2.js
    Added function, showed fullName and evoked.

---
+++ module-3/lesson-2/scripts/task-3.2.2.js
@@ -0,0 +1,8 @@
+function showFullName(fName,sName){
+var fullName = fName + sName;
+document.write(  fullName + '<br>' );
+}
+
+showFullName('Charlotte', 'Croshaw');
+showFullName('Sam' 'Smith');
+showFullName('Demi' 'Moore');
