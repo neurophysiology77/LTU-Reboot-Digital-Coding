f6be555632a3cef4f7e056061371b2a5acc373e6
neurophysiology77 <77329599+neurophysiology77@users.noreply.github.com>
2021-01-26 10:50:33 +0000

    Update task-3.2.3.js
    Add showName function and return value for use in programme.

--- module-3/lesson-2/scripts/task-3.2.3.js
+++ module-3/lesson-2/scripts/task-3.2.3.js
@@ -11,4 +11,14 @@ var message = greet('Hello', 'Luke');
 
 // ADD YOUR CODE BELOW HERE
 
+function showFullName(fName,sName){
+  var fullName = fName + '   ' + sName;
+  return  fullName;
+  }
+
+  var person1 = showFullName('Charlotte, 'Croshaw')
+  document.write( person1);
+  
+  var person2 = showFullName('Demi', 'Moore');
+  document.write(   person2);
 // ADD YOUR CODE ABOVE HERE
