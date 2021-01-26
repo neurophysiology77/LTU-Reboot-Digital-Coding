0eb0d7d4c5e517ba89b32010927317e83f1f7862
neurophysiology77 <77329599+neurophysiology77@users.noreply.github.com>
2021-01-26 12:27:00 +0000

    Update task-3.2.5.js
    Corrected task from earlier due to error message.

---
+++ module-3/lesson-2/scripts/task-3.2.5.js
@@ -0,0 +1,19 @@
+var temperature = 49;
+
+if (temperature < 50) {
+document.write('Put on a coat'); 
+}
+
+var temperature = 29;
+
+if (temperature < 30) {
+document.write('Wear a coat and a hat'); 
+}
+
+else if (temperature < 0) {
+document.write('Stay inside please'); 
+}
+
+else {
+document.write('Just pants and vest is fine'); 
+}
