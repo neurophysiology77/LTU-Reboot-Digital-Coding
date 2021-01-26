e8a0ce6d6b0e20d780bbc512d35ae9915bdd6b23
neurophysiology77 <77329599+neurophysiology77@users.noreply.github.com>
2021-01-26 10:01:07 +0000

    Update task-3.2.1.js
    Declared function message and invoked message function.

---
+++ module-3/lesson-2/scripts/task-3.2.1.js
@@ -0,0 +1,7 @@
+// Declare a function
+function koalaFacts ( ) {
+    console.log('Some koalas are very cute');
+    console.log('Koalas can harbour a number of diseases however');
+}
+// Invoke a function
+koalaFacts();
