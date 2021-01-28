b34fb1668c89926f812d2bf3ceb55ec6ff6b48ae
neurophysiology77 <77329599+neurophysiology77@users.noreply.github.com>
2021-01-28 13:49:15 +0000

    Create task-3.4.2.js
    Added an array and looped over it using a 'for' loop.

---
+++ module-3/lesson-4/Scripts/task-3.4.2.js
@@ -0,0 +1,7 @@
+var faveFoods = ['Chocolate', 'Apricots', 'Thai Green Curry', 'Bannoffee Pie'];
+
+for (var index = 0; index < faveFoods.length; index++) {
+
+    console.log( faveFoods [index]);
+
+}
